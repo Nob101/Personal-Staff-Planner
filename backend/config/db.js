5c870e6c4459063a17cca2ce34ea7022435e0ff5
Lukas <lukas.586@hotmail.com>
2025-10-19 12:02:45 +0200

    postgresql installiert (Abhängigkeiten-> package.json); kommentare -> db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,10 @@
+
+// Importiert die 'Pool' Klasse aus dem 'pg' Modul
 import { Pool } from 'pg';
 
+
+// Neue Pool Instanz inklusive Datenbankerbindung (Einstellungen)
+// User ; IP der Datenbank; Datenbank inkl Passwort; Port für DB-zugang
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
