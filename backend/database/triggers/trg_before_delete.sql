ecb524c6bb0a50c2ab7e0552aa4f9bca4fd7fae6
Lukas <lukas.586@hotmail.com>
2025-11-14 15:41:20 +0100

    triggeer extra senden

---
+++ backend/database/triggers/trg_before_delete.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TRIGGER trg_warn_delete
+BEFORE DELETE ON mitarbeiter
+FOR EACH ROW
+EXECUTE FUNCTION fn_warn_before_delete();
+
+-- Das fenster muss im Frontend erstellt werden [gerendert] !!!!
+
+CREATE TRIGGER trg_warn_delete
+BEFORE DELETE ON filiale
+FOR EACH ROW
+EXECUTE FUNCTION fn_warn_before_delete();
+
+
+
