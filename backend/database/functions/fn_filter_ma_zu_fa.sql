1ad293c97dc9aa1f2530dc7ab0561d50a5043c40
Lukas <lukas.586@hotmail.com>
2025-11-14 09:08:43 +0100

    Vorläufige Funktionen

---
+++ backend/database/functions/fn_filter_ma_zu_fa.sql
@@ -0,0 +1,24 @@
+
+-- Listet alle MA die in einer Filaile zugeteilt sind
+CREATE OR REPLACE FUNCTION fn_list_employees_by_filiale(fnr INTEGER)
+RETURNS TABLE (
+  mnr INTEGER,
+  vorname VARCHAR,
+  nachname VARCHAR,
+  fkurzl VARCHAR,
+  akurzl VARCHAR
+) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT m.MNr, m.Vorname, m.Nachname, m.FKurzl, m.AKurzl
+  FROM mitarbeiter m
+  JOIN mitarbeiter_arbeitet_in_Filiale mf ON m.MNr = mf.MNr
+  WHERE mf.FNr = fnr;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Aufruf im Node.js file mit...
+-- const { rows } = await pool.query('SELECT * FROM fn_list_employees_by_filiale($1)', [fnr]);
+
+
