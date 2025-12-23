0edf2cc3097fe957d9a2829ce4fe94072a6e00aa
Lukas <lukas.586@hotmail.com>
2025-12-23 12:01:51 +0100

    Datenbank server.js .env trigger functions erstellt

--- backend/db/database/scripts/user_role.sql
+++ backend/db/database/scripts/user_role.sql
@@ -5,8 +5,13 @@ CREATE ROLE admin LOGIN PASSWORD 'load_solution' SUPERUSER INHERIT CREATEROLE NO
 
 -- Grant alle Rechte auf die Datenbank (ersetze dienstplan durch deinen DB-Namen)
 GRANT CONNECT ON DATABASE dienstplan TO admin;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO markus;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO markus;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
+
+
+-- Damit der Admin später bei bedarf rechte vergebn kann
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO admin;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO admin;
 
 -- Gastbenutzer anlegen mit nur Leserechten
 --kein sudo r--
@@ -15,8 +20,10 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO markus;
 CREATE ROLE gast LOGIN PASSWORD '12gast34' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
 
 
+
 -- Leserechte gewähren
 GRANT CONNECT ON DATABASE dienstplan TO gast;
+GRANT USAGE ON SCHEMA public TO gast;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO gast;
 
 
--- backend/db/database/triggers/trg_before_delete.sql
+++ backend/db/database/triggers/trg_before_delete.sql
@@ -1,13 +1,15 @@
 
+DROP TRIGGER IF EXISTS trg_warn_delete_mitarbeiter ON mitarbeiter;
+DROP TRIGGER IF EXISTS trg_warn_delete_filiale ON filiale;
 
-CREATE TRIGGER trg_warn_delete
+CREATE TRIGGER trg_warn_delete_mitarbeiter
 BEFORE DELETE ON mitarbeiter
 FOR EACH ROW
 EXECUTE FUNCTION fn_warn_before_delete();
 
 -- Das fenster muss im Frontend erstellt werden [gerendert] !!!!
 
-CREATE TRIGGER trg_warn_delete
+CREATE TRIGGER trg_warn_delete_filiale
 BEFORE DELETE ON filiale
 FOR EACH ROW
 EXECUTE FUNCTION fn_warn_before_delete();
