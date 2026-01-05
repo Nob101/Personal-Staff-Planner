2eb6775e70c40c965352112968bdc5d530dba535
Lukas <lukas.586@hotmail.com>
2026-01-05 13:50:28 +0100

    Dockerfiles und Procedure angelegt/ test erfolgreich

--- backend/db/database/functions/fn_update_timestamp.sql
+++ backend/db/database/functions/fn_update_timestamp.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE FUNCTION fn_update_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.erstellt_am = NOW();  --bei update aktualisieren
+    NEW.aktualisiert_am = current_timestamp;  --bei update aktualisieren
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
