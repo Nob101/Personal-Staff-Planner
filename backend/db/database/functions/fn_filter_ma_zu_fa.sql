834f278ab30972b41de90a22ca2d4cfd5e0b35d9
Lukas <lukas.586@hotmail.com>
2026-01-05 13:50:28 +0100

    Dockerfiles und Procedure angelegt/ test erfolgreich

--- backend/db/database/functions/fn_filter_ma_zu_fa.sql
+++ backend/db/database/functions/fn_filter_ma_zu_fa.sql
@@ -1,5 +1,6 @@
 
 -- Listet alle MA die in einer Filaile zugeteilt sind
+-- !!! An aktuelle DB angepasst !!!
 CREATE OR REPLACE FUNCTION fn_list_employees_by_filiale(fnr INTEGER)
 RETURNS TABLE (
   mnr INTEGER,
@@ -11,7 +12,8 @@ BEGIN
   RETURN QUERY
   SELECT m.MNr, m.Vorname, m.Nachname, m.AKurzl
   FROM mitarbeiter m
-  JOIN mitarbeiter_arbeitet_in_Filiale mf ON m.MNr = mf.MNr
+  JOIN mitarbeiter_arbeitet_in_Filiale mf ON m.mnr = mf.mnr
+  LEFT JOIN filiale f ON m.hauptfiliale_fnr = f.fnr
   WHERE mf.FNr = fnr;
 END;
 $$ LANGUAGE plpgsql;
--- backend/db/database/indexes/main_indexes.sql
+++ backend/db/database/indexes/main_indexes.sql
@@ -1,18 +1,21 @@
 
 
--- Dienstplan-Suche Performance
+
+-- Dienstplan-Suche -> Performance
 
 CREATE INDEX IF NOT EXISTS idx_dienstplaene_datum ON dienstplaene(fnr, datum);
 CREATE INDEX IF NOT EXISTS idx_dienstplaene_jahr_monat ON dienstplaene(jahr, monat);
 
 -- Mitarbeiter filterung nach Filialen
 CREATE INDEX IF NOT EXISTS idx_mitarbieter_hauptfiliale ON mitarbeiter(hauptfiliale_fnr);
-
 CREATE INDEX IF NOT EXISTS idx_dienstplaene_mnr_datum ON dienstplaene(mnr, datum);
+
 
 -- Schnellsuche nach Mitarbeitern, die als Springer (true) markiert sind
 CREATE INDEX IF NOT EXISTS idx_mitarbeiter_springer ON mitarbeiter(springer) WHERE springer = TRUE;
 
+-- Performance für M:N Tabelle (Algo prüft häufig die MA verfügbarkeit)
+CREATE INDEX IF NOT EXISTS idx_ma_filiale_fnr ON mitarbeiter_arbeitet_in_filiale(fnr);
 
 
 -- Performance für joins (foreign Keys)
