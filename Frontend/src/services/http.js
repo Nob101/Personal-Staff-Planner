80cf484e0a493408ed35f883eaf1702766f9746d
Dumitru Jelezneac <dumitru.jelezneac@htlpinkfafeld.at>
2026-01-08 10:28:48 +0100

    Aktualisierung vom Main

--- Frontend/src/services/http.js
+++ Frontend/src/services/http.js
@@ -4,7 +4,7 @@
 import axios from "axios"
 
 export const http = axios.create({
-  baseURL: "http://localhost:3001", // JSON Server Base URL Muss mit Backend URL ersetzt werden.Heißt: http://localhost:3000/api/
+  baseURL: "http://localhost:3001/api/", // JSON Server Base URL Muss mit Backend URL ersetzt werden.Heißt: http://localhost:3000/api/
   headers: {
     "Content-Type": "application/json"
   }
