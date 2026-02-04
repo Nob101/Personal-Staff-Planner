9bdfc6cf98759f87845bd3e31dcd4123e5aeb84a
Lukas <lukas.586@hotmail.com>
2026-02-04 07:28:21 +0100

    NEW: Bind-Mount der Volumes / Fix: Export-Funktion

--- backend/functions/cleanUpService.js
+++ backend/functions/cleanUpService.js
@@ -3,14 +3,17 @@
  * Lukas Atzmüller
  * INTERVAL  -> in Postgres ein spezieller Datentyp für Zeitspannen
  * mit 
- *   - Intervall '13 months' rechnet Postgres im Kalender zurück.
+ *      Intervall '13 months' rechnet Postgres im Kalender zurück.
  *      Vorteil: Schaltjahre und Monatslängen werden Automatisch berücksichtigt
  *      und CURRENT_DATE nimmt die Uhrzeit des Servers = sehr präzise
+ * 
+ * NEU: Transaktion um Karteileichen (inaktive MA/FA) mit zu entfernen
  */
 
 const pool = require('../db/pool');
 
 const deleteOldShifts = async () => {
+    const client = await pool.connect();
     try {
 
         // NEU: Nur bei zuvielen Rows ausführen. Gelöscht wird ab einem Zeitstempel
@@ -18,24 +21,44 @@ const deleteOldShifts = async () => {
         const totalRows = parseInt(count.rows[0].count);
 
         if(totalRows > 170000){
+            await client.query('BEGIN');
 
-             const query = `
+            const shiftDelete = await client.query (`
             DELETE FROM dienstplaene
             WHERE datum < CURRENT_DATE - INTERVAL '13 months';
-        `;
+        `) ;
 
-        const result = await pool.query(query);
-        console.log(`Cleanup erfolgreich: ${result.rowCount} alte Schichten gelöscht.`);
-     
+        // NEU: Inaktive MA löschen EXISTS macht es 'fehlerfrei'
+           const maDelete = await client.query(`
+                DELETE FROM mitarbeiter 
+                WHERE aktiv = FALSE 
+                AND NOT EXISTS (SELECT 1 FROM dienstplaene WHERE mnr = mitarbeiter.mnr);
+            `);     
+            // Inaktive Filailen löschen
+            const filialeDelete = await client.query(`
+                DELETE FROM filiale 
+                WHERE aktiv = FALSE 
+                AND NOT EXISTS (SELECT 1 FROM dienstplaene WHERE fnr = filiale.fnr);
+            `);
+
+            await client.query('COMMIT');
+
+
+       
         } else {
             console.log(`Cleanup übersprungen: Aktuell nur ${totalRows} Zeilen.`);
         }
 
        
     } catch (err) {
-        console.error('Fehler beim automatischen Cleanup:', err.message);
+        await client.query('ROLLBACK');
+        console.error('Fehler beim automatischen Cleanup (Rollback ausgeführt):', err.message);
+    } finally {
+        client.release();       //NEU:  Verbindung zurück in den Pool geben
     }
   
 };
 
 module.exports = { deleteOldShifts };
+
+
