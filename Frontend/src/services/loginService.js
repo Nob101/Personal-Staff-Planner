7f699d832785e4f97497c75252bc6f18d3678137
Dumitru Jelezneac <dumitru.jelezneac@htlpinkfafeld.at>
2026-02-18 07:11:28 +0100

    erste 1366x768 Anpassungen

--- Frontend/src/services/loginService.js
+++ Frontend/src/services/loginService.js
@@ -1,20 +1,12 @@
-//loginServie.js
-//Hier ist gesamte Backend-Kommunikation für Login/Auth
+// loginServie.js
+// Gesamte Backend-Kommunikation für Login
 
 //BaseURL importieren
 import { http } from './http'
 
-// --- Login ---
+// Login
 export function login(benutzername, passwort) {
   return http.post('/auth/login', {
-    username: benutzername,
-    password: passwort
-  })
-}
-
-// --- Registrierung ---
-export function register(benutzername, passwort) {
-  return http.post('/auth/register', {
     username: benutzername,
     password: passwort
   })
